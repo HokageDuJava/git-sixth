0fdf3bf853518d51b6d2d390d2df51e66e1eebea
arzad <arzad.aboubacar@gmail.com>
2020-04-16 12:09:56 +0200

    post of an API added on the about page

--- src/navbar/About.js
+++ src/navbar/About.js
@@ -1,17 +1,48 @@
 import React from "react";
+import axios from "axios";
+class About extends React.Component {
+  state = {
+    posts: [],
+  };
 
-const About = (props) => {
-  return (
-    <div className="container">
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
-        tempore hic vero quaerat possimus placeat et quibusdam ad magnam illum,
-        optio molestias nam, molestiae nemo provident nisi eius! Veritatis,
-        consequuntur?
-      </p>
-    </div>
-  );
-};
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((posts) => {
+      this.setState({
+        posts: posts.data.slice(1, 10),
+      });
+    });
+  }
+
+  render() {
+    const { posts } = this.state;
+    const postsList = posts.length ? (
+      posts.map((post) => {
+        return (
+          <div className="card" key={post.id}>
+            <h1 className="card-title">{post.title}</h1>
+            <p className="card-content">{post.body}</p>
+          </div>
+        );
+      })
+    ) : (
+      <div className="card">
+        <p className="card-content">There is no post</p>
+      </div>
+    );
+
+    return (
+      <div className="container">
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
+          tempore hic vero quaerat possimus placeat et quibusdam ad magnam
+          illum, optio molestias nam, molestiae nemo provident nisi eius!
+          Veritatis, consequuntur?
+        </p>
+        {postsList}
+      </div>
+    );
+  }
+}
 
 export default About;
